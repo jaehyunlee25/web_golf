8d1ff92a3b33c9ecaf834ab67632194b89d1a9c9
jaehyunlee <jaehyunlee25@gmail.com>
2022-02-04 13:05:26 +0900

    renew

---
+++ public/js/schedule.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-extend-native */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-alert */
+/* eslint-disable new-cap */
+/* eslint-disable camelcase */
+/* eslint-disable func-names */
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-undef */
+/* eslint-disable prefer-template */
+const cf = new jCommon();
+const ADDR_HEADER = 'http://dev.mnemosyne.co.kr:1006';
+setClubs();
+
+selClubs.onchange = function () {
+  const info = JSON.parse(this.value);
+  elAddr.innerHTML = info.address;
+  elCorpNum.innerHTML = info.corp_reg_number;
+  elPhone.innerHTML = info.phone;
+  elEmail.innerHTML = info.email;
+  btnSearch.onclick();
+};
+btnSearch.onclick = function () {
+  const addr = ADDR_HEADER + '/api/reservation/getSchedule';
+  const info = JSON.parse(selClubs.value);
+  post(
+    addr,
+    { golf_club_id: info.id },
+    { 'Content-Type': 'application/json' },
+    (data) => {
+      const result = JSON.parse(data);
+      mkTable(result.schedule);
+    },
+  );
+};
+function mkTable(schedule) {
+  divMain.innerHTML = '';
+  Object.keys(schedule).forEach((date) => {
+    const elDate = document.createElement("div");
+    divMain.appendChild(elDate);
+    divMain.style.paddingLeft = 120 + 'px';
+    elDate.innerHTML=date;
+    elDate.style.marginTop = 20 + 'px';
+    
+    const obDate = schedule[date];
+    Object.keys(obDate).sort().forEach((course) => {
+      const elCourse = document.createElement("div");
+      elDate.appendChild(elCourse);
+      elCourse.innerHTML=course;
+      elCourse.style.paddingLeft = 10 + 'px';
+      elCourse.style.marginTop = 10 + 'px';
+      
+      const obCourse = obDate[course];
+      Object.keys(obCourse).sort().forEach((timeSlot) => {
+        const elTimeSlot = document.createElement("div");
+        elCourse.appendChild(elTimeSlot);
+        elTimeSlot.innerHTML=timeSlot;
+        elTimeSlot.style.paddingLeft = 10 + 'px';
+
+        const arTimeSlot = obCourse[timeSlot];
+        arTimeSlot.forEach((ob) => {
+          const elTime = document.createElement("span");
+          elTimeSlot.appendChild(elTime);
+          elTime.innerHTML=ob.time.ct(3);
+          elTime.style.paddingLeft = 10 + 'px';
+        });
+      });
+    });
+  });
+};
+function setClubs() {
+  const addr = ADDR_HEADER + '/api/reservation/getGolfClubs';
+  post(addr, {}, { 'Content-Type': 'application/json' }, (data) => {
+    const result = JSON.parse(data);
+    dir(result);
+    selClubs.innerHTML = '';
+    result.golfClubs.forEach((obj) => {
+      const opt = document.createElement('option');
+      selClubs.appendChild(opt);
+      opt.value = JSON.stringify(obj);
+      opt.innerHTML = obj.name;
+    });
+    selClubs.onchange();
+  });
+}
