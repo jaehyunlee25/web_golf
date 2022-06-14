7856d81458087027aa1380769d43c6b3a68d67e0
jaehyunlee <jaehyunlee25@gmail.com>
2022-06-14 23:33:22 +0900

    renew

--- public/js/control.js
+++ public/js/control.js
@@ -14,126 +14,166 @@
 
 // const cf = new jCommon();
 const ADDR_HEADER = 'http://dev.mnemosyne.co.kr:1006';
+const WS_HEADER = 'ws://dev.mnemosyne.co.kr:9001';
+const socket = new WebSocket(WS_HEADER);
+socket.onopen = wsopen;
+socket.onmessage = wsmessage;
+socket.onclose = wsclose;
+socket.onerror = wserror;
 let count = 0;
 
 setClubs();
 
+function wsopen() {
+  console.log('socket server opened!');
+  socket.send(
+    JSON.stringify({
+      command: 'subscribe',
+      topic: 'TZLOG',
+    }),
+  );
+}
+function wsclose(event) {
+  console.log('socket server cloded!', event.code, envent.reason);
+}
+function wserror(e) {
+  console.log(e.message);
+}
+function wsmessage(event) {
+  let json;
+  try {
+    json = JSON.parse(event.data);
+    json.message = JSON.parse(json.message);
+    if (json.message.subType == 'search')
+      json.message.parameter = JSON.parse(json.message.parameter);
+    const param = json.message.parameter;
+    if (param.order == param.total)
+      console.log('search end:', json.messsage.golfClubId);
+  } catch (e) {
+    console.log(event.data);
+  }
+}
 function btnclick() {
   this.disabled = 'disabled';
   this.innerHTML = '조회중';
   const addr = 'http://mnemosynesolutions.co.kr:8080/control';
   console.log(this.club);
-  post(addr, { club: this.club }, { 'Content-Type': 'application/json' }, data => {
-    console.log(data);
-    this.disabled = false;
-    this.innerHTML = 'search';
-  });
-};
+  post(
+    addr,
+    { club: this.club },
+    { 'Content-Type': 'application/json' },
+    (data) => {
+      console.log(data);
+      this.disabled = false;
+      this.innerHTML = 'search';
+    },
+  );
+}
 function timer() {
   count = boxes.children.length - 1;
   getSchedule();
-  const t = setInterval(() =>{
-    log("timer");
+  /* const t = setInterval(() => {
+    log('timer');
     count = boxes.children.length - 1;
     getSchedule();
-  }, 1000 * 10);
-};
+  }, 1000 * 10); */
+}
 function getSchedule() {
   const lmt = boxes.children.length - 1;
   const box = boxes.children[lmt - count];
   const club_id = box.id;
-  const addr = ADDR_HEADER + '/api/reservation/getSchedule';  
+  const addr = ADDR_HEADER + '/api/reservation/getSchedule';
   post(
     addr,
     { golf_club_id: club_id },
     { 'Content-Type': 'application/json' },
     (data) => {
       const result = JSON.parse(data);
-      const time = (new Date() - new Date(result.timeStamp));
+      const time = new Date() - new Date(result.timeStamp);
       let tDate = time.ago();
-      if(!tDate) tDate = "조회불가";
-      box.children[2].innerHTML = "조회: " + tDate;
-      box.style.cssText = "background-color:" + time.getColor();
+      if (!tDate) tDate = '조회불가';
+      box.children[2].innerHTML = '조회: ' + tDate;
+      box.style.cssText = 'background-color:' + time.getColor();
       count--;
-      if(count < 0) return;
+      if (count < 0) return;
       getSchedule();
     },
   );
-};
+}
 function setClubs() {
   const addr = ADDR_HEADER + '/api/reservation/getGolfClubs';
   post(addr, {}, { 'Content-Type': 'application/json' }, (data) => {
     const result = JSON.parse(data);
-    
-    result.golfClubs.sort((a, b) => { 
-      if(a.name > b.name) return 1;
-      if(a.name < b.name) return -1;
+
+    result.golfClubs.sort((a, b) => {
+      if (a.name > b.name) return 1;
+      if (a.name < b.name) return -1;
       return 0;
     });
 
     dir(result.golfClubs);
 
-    result.golfClubs.forEach((obj) => {      
+    result.golfClubs.forEach((obj) => {
       const box = boxes.add('div');
-      box.className = "box";
+      box.className = 'box';
       box.id = obj.id;
       box.obj = obj;
 
       const name = box.add('div');
-      name.className = "name";
+      name.className = 'name';
       name.innerHTML = obj.name;
-      
+
       const reg = box.add('div');
-      reg.className = "reg";
-      reg.innerHTML = "등록: " + (new Date() - new Date(obj.created_at)).ago();
+      reg.className = 'reg';
+      reg.innerHTML = '등록: ' + (new Date() - new Date(obj.created_at)).ago();
 
       const last = box.add('div');
-      last.className = "reg";
-      last.innerHTML = "조회: ";
+      last.className = 'reg';
+      last.innerHTML = '조회: ';
 
       const btnCover = box.add('div');
-      btnCover.style.textAlign = "center";
+      btnCover.style.textAlign = 'center';
       const btn = btnCover.add('button');
-      btn.innerHTML = "search";
+      btn.innerHTML = 'search';
       btn.club = obj.eng_id;
       btn.onclick = btnclick;
-    });    
+    });
     timer();
   });
 }
-Number.prototype.getColor = function() {
+Number.prototype.getColor = function () {
   const cf = new jCommon();
   const day = 1000 * 60 * 60 * 24;
   const hour = 1000 * 60 * 60;
   const min = 1000 * 60;
-  if(this > day) {
-    return "firebrick"
-  } else if(this > hour) {
-    return "lightgreen"
-  } else if(this > min) {
-    return "greenyellow"
-  } else if(this <= min) {
-    return "greenyellow"
+  if (this > day) {
+    return 'firebrick';
+  } else if (this > hour) {
+    return 'lightgreen';
+  } else if (this > min) {
+    return 'greenyellow';
+  } else if (this <= min) {
+    return 'greenyellow';
   } else {
-    return "red"
+    return 'red';
   }
 };
-Number.prototype.ago = function() {
+Number.prototype.ago = function () {
   const cf = new jCommon();
   const day = 1000 * 60 * 60 * 24;
   const hour = 1000 * 60 * 60;
   const min = 1000 * 60;
-  if(this > day) {
-    return cf.rommify(this / day, 0) + " 일 전"
-  } else if(this > hour) {
-    return cf.rommify(this / hour, 0) + " 시간 전"
-  } else if(this > min) {
-    return cf.rommify(this / min, 0) + " 분 전"
-  } else if(this <= min) {
-    return cf.rommify(this / 1000, 0) + " 초 전"
+  if (this > day) {
+    return cf.rommify(this / day, 0) + ' 일 전';
+  } else if (this > hour) {
+    return cf.rommify(this / hour, 0) + ' 시간 전';
+  } else if (this > min) {
+    return cf.rommify(this / min, 0) + ' 분 전';
+  } else if (this <= min) {
+    return cf.rommify(this / 1000, 0) + ' 초 전';
   }
 };
-HTMLElement.prototype.add = function(tag) {
+HTMLElement.prototype.add = function (tag) {
   const elm = document.createElement(tag);
   this.appendChild(elm);
   return elm;
