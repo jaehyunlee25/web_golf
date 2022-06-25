0c000a374e05c8f0fd7e48ef53e2e2ef4edc11da
jaehyunlee <jaehyunlee25@gmail.com>
2022-06-25 17:12:08 +0900

    renew

--- public/js/loginEditor.js
+++ public/js/loginEditor.js
@@ -14,7 +14,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-undef */
 /* eslint-disable prefer-template */
-String.prototype.proc = function() {
+String.prototype.proc = function () {
   return this.replaceAll('\r', '');
 };
 const cf = new jCommon();
@@ -25,39 +25,40 @@ const WS_HEADER = 'ws://dev.mnemosyne.co.kr:9001';
 const golf_club_id = cf.getGet().club_id;
 log(golf_club_id);
 post(
-  "http://mnemosynesolutions.co.kr:8080/get_pure_search_core", 
-  { club: 'tgv_KMH' }, 
-  {'Content-Type': 'application/json'}, 
-  data => {
-    const json = JSON.parse(data);
+  'http://mnemosynesolutions.co.kr:8080/get_pure_login',
+  { club: 'tgv_KMH' },
+  { 'Content-Type': 'application/json' },
+  (data) => {
+    console.log(data);
+    /* const json = JSON.parse(data);
     dir(json);
     taMneCall.value = json.part.mneCall.proc();
     taMneCallDetail.value = json.part.mneCallDetail.proc();
     taFunction.value = json.part.function.proc();
-    taCommand.value = json.part.command.proc();
-  }
+    taCommand.value = json.part.command.proc(); */
+  },
 );
 /* getSchedule(golf_club_id, (data) => {
   dir(data);
 }); */
-btnSubmit.onclick = function() {
+btnSubmit.onclick = function () {
   const part = {
-    mneCall: "",
-    mneCallDetail: "",
-    function: "",
-    command: "",
+    mneCall: '',
+    mneCallDetail: '',
+    function: '',
+    command: '',
   };
   part.mneCall = taMneCall.value.proc();
   part.mneCallDetail = taMneCallDetail.value.proc();
   part.function = taFunction.value.proc();
   part.command = taCommand.value.proc();
   post(
-    "http://mnemosynesolutions.co.kr:8080/set_pure_search_core", 
-    { club: 'tgv_KMH', part }, 
-    {'Content-Type': 'application/json'}, 
-    data => {
+    'http://mnemosynesolutions.co.kr:8080/set_pure_search_core',
+    { club: 'tgv_KMH', part },
+    { 'Content-Type': 'application/json' },
+    (data) => {
       log(data);
-    }
+    },
   );
 };
 function getSchedule(golf_club_id, callback) {
@@ -67,7 +68,7 @@ function getSchedule(golf_club_id, callback) {
     { golf_club_id },
     { 'Content-Type': 'application/json' },
     (data) => {
-      if(callback) callback(JSON.parse(data));
-    }
-  );    
+      if (callback) callback(JSON.parse(data));
+    },
+  );
 }
