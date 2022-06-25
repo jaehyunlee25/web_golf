6ba84b0d61b86243fcc3972103de018aa6c2aba8
jaehyunlee <jaehyunlee25@gmail.com>
2022-06-25 16:41:33 +0900

    renew

---
+++ public/js/newLogin.js
@@ -0,0 +1,106 @@
+/* eslint-disable no-self-assign */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-shadow */
+/* eslint-disable no-useless-concat */
+/* eslint-disable eqeqeq */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-else-return */
+/* eslint-disable consistent-return */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-extend-native */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-alert */
+/* eslint-disable new-cap */
+/* eslint-disable camelcase */
+/* eslint-disable func-names */
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-undef */
+/* eslint-disable prefer-template */
+const cf = new jCommon();
+const ADDR_HEADER = 'http://dev.mnemosyne.co.kr:1006';
+const addr = ADDR_HEADER + '/api/reservation/getGolfClubLoginInfo';
+post(
+    addr,
+    { },
+    { 'Content-Type': 'application/json' },
+    (data) => {
+        const result = JSON.parse(data);
+        mkTable(result.golfClubs);
+    }
+);
+function mkTable(result){
+    result.forEach(obj => {
+        const tr = tblLogin.add('tr');
+        let flg = false;
+        const ipts = [];
+        Object.keys(obj).forEach(key => {
+            if(key == 'created_at' || key == 'updated_at') return;
+            const val = obj[key];
+            const tdd = tr.add("td");
+            const td = tdd.add('div');
+            td.innerHTML = val;
+            let ipt;
+            if(key == 'homepage' || key == 'mobile'){
+                td.style.width = 450 + "px";
+                if(val != null){
+                    td.innerHTML = '';
+                    const a = td.add('a');
+                    a.href = val;
+                    a.innerHTML = val;
+                    a.target = '_blank';
+                    td.style.textAlign = 'left';
+                }
+            }
+            if(val == null || val == "") {
+                ipt = td.add("input");
+                ipt.style.width = '90%';
+                ipt.name = key;
+                flg = true;
+                ipts.push(ipt);
+            }
+        });
+        const btnTd = tr.add('td');
+        if(!flg) return;
+        const btn = btnTd.add('button');
+        btn.innerHTML = '입력';
+        btn.param = obj;
+        btn.ipts = ipts;
+        btn.style.width = '80px';
+        btn.onclick = btnclick;
+
+        const editTd = tr.add('td');
+        const btnEdit = editTd.add('button');
+        btnEdit.innerHTML = '편집';
+        btnEdit.param = obj;
+        btnEdit.ipts = ipts;
+        btnEdit.style.width = '80px';
+        btnEdit.onclick = btnEditclick;
+    });
+};
+function btnEditclick() {
+  location.href = "loginEditor.html?golfClubId=" + this.param.id;
+};
+function btnclick() {
+    dir(this.ipts);
+    
+    this.ipts.forEach(ipt => {
+        this.param[ipt.name] = ipt.value;
+    });
+    
+    dir(this.param);
+    const addr = ADDR_HEADER + '/api/reservation/setGolfClubLoginInfo';
+    post(
+        addr,
+        this.param,
+        { 'Content-Type': 'application/json' },
+        (data) => {
+            const result = JSON.parse(data);
+            location.href = location.href;
+        }
+    );
+};
+HTMLElement.prototype.add = function(tagName){
+    const el = document.createElement(tagName);
+    this.appendChild(el);
+    return el;
+};
